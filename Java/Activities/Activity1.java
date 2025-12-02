c9b6cb00de8cacf9c962835ba1c643121662c84d
Slizaroy <slizaroy@in.ibm.com>
2025-12-02 13:25:46 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,18 @@
+package Activities;
+
+public class Activity1 {
+	public static void main(String[] args) {
+	Car icon= new Car();
+	icon.make=2014;
+	icon.color="black";
+	icon.trans="Manual";
+	
+	icon.displayCharacterstics();
+    icon.accelerate();
+    icon.brake();
+}
+
+}
+	
+
+
