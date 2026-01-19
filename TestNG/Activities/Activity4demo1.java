ea9837fde24ee94e48f97616e38c51863c9bc830
Sliza Roy <slizaroy@in.ibm.com>
2026-01-19 18:52:07 +0530

    uploaded testng activities

--- TestNG/Activities/Activity4demo1.java
+++ TestNG/Activities/Activity4demo1.java
@@ -1,5 +1,16 @@
 package examples;
 
-public class Activity4demo1 {
 
+import org.testng.annotations.Test;
+
+public class Activity4demo1 {
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in first test case from demoOne Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in second test case from demoOne Class");
+    }
 }
